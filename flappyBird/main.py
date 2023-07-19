df0c089835556ab224d96689a4a7b39e676cc50a
Victoria Moya Anna Ceccaroni <victoriaceccaroni@gmail.com>
2023-07-19 22:12:08 +0200

    fixed fit of assets on screen

--- flappyBird/main.py
+++ flappyBird/main.py
@@ -3,19 +3,26 @@ import random
 
 pygame.init()
 
+#initialise game screen
+gameScreen = pygame.display.set_mode((400, 750)) #set_mode(width, height)
+screenWidth, screenHeight = gameScreen.get_size()
+FPS = 50 #Frames per second
+
 #create variables and set paths to assets
 background = pygame.image.load('assets/townsville.png')
+background = pygame.transform.scale(background, (screenWidth, screenHeight))
 bird = pygame.image.load('assets/buttercup.png')
+bird = pygame.transform.scale(bird, (100, 100))  # example bird size, change as necessary
 base = pygame.image.load('assets/base.png')
+base = pygame.transform.scale(base, (screenWidth, screenHeight))
 gameover = pygame.image.load('assets/gameover.png')
+gameover = pygame.transform.scale(gameover, (screenWidth, screenHeight))
 highTube = pygame.image.load('assets/highTube.png')
+highTube = pygame.transform.scale(highTube, (100, 300))  # example tube size, change as necessary
 lowTube = pygame.image.load('assets/lowTube.png')
+lowTube = pygame.transform.scale(lowTube, (100, 300))
 
-#initialise game screen
-gameScreen = pygame.display.set_mode((288, 512)) #set_mode(width, height)
-FPS = 50 #Frames per second
 #adding elements to screen
-
 def init():
     global birdx, birdy, birdVelY
     birdx, birdy = 60, 150
